c98f2cceb3c337114f44555cca7084125c319bc6
ali <@alihagh>
2023-10-05 20:02:34 +0330

    here is a part of quick transfer box

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import React from "react";
 import styles from "./App.module.scss";
 import Navbar from "./scss/layout/Navbar";
 import Sidebar from "./scss/layout/Sidebar";
+import QuickTransfer from "./components/QuickTransfer";
 
 const App = () => {
   return (
     <div className={styles.container}>
-      <Sidebar />
-      <Navbar />
+<Navbar />
+<main>
+
+       <Sidebar className={styles.Sidebar}/>
+       <QuickTransfer className={styles.Transfer}/>
+</main>
+
     </div>
   );
 };
