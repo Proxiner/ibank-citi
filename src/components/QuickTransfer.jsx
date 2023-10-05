920b686d725086c05e23b32f6dca9b66adcb9a4e
ali <@alihagh>
2023-10-05 20:32:04 +0330

    pushing my codes

--- src/components/QuickTransfer.jsx
+++ src/components/QuickTransfer.jsx
@@ -8,7 +8,10 @@ function QuickTransfer() {
 
       <div className={styles.quickTransferBox}> 
      <h1 className={styles.Slider}>Quick Money Transfer</h1>
-       <div className={styles.cardBox}><img src={Navigation} alt="its just a button LOL" className={styles.slider}/></div> 
+       <div className={styles.cardBox}>
+        <img src={Navigation} alt="its just a button LOL" className={styles.slider}/>
+        <span>5859 ***** ***** 2245</span>
+        </div> 
       </div>
 
     </>
