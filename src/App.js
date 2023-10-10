cd4994fd57286966a12a44e3317e35b664ddc386
Erfandev88 <gharibi69068@gmail.com>
2023-10-10 16:22:50 +0330

    Sidebar and Navbar place fixed!

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from "react";
 
 import styles from "./App.module.scss";
-// import Navbar from "./scss/layout/Navbar";
-// import Sidebar from "./scss/layout/Sidebar";
+import Navbar from "./scss/layout/Navbar";
+import Sidebar from "./scss/layout/Sidebar";
 import QuickTransfer from "./components/QuickTransfer";
 
 const App = () => {
   return (
     <div className={styles.container}>
-      {/* <Navbar />  */}
-      {/* <main> */}
+      <Navbar />
+      <main className={styles.main}>
 
-      {/* <Sidebar className={styles.Sidebar}/>  */}
-      <QuickTransfer className={styles.Transfer} />
-      {/* </main>  */}
+        <Sidebar className={styles.Sidebar} />
+        <QuickTransfer className={styles.Transfer} />
+      </main>
 
     </div>
   );
