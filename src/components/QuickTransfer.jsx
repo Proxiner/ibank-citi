c852e1c72d6e9fdf189ee807e0f62c8dabcfcf76
ali <@alihagh>
2023-10-10 16:15:45 +0330

    credit card error has been fixed

--- src/components/QuickTransfer.jsx
+++ src/components/QuickTransfer.jsx
@@ -1,19 +1,14 @@
 import React from 'react'
-import Navigation from '../assets/navigation.svg'
 import styles from "./Transfer.module.scss"
-import Tejarat from "../assets/Tejarat-Bank-logo.svg"
+import CreditCardNumber from './CreditCardNumber'
 
-function QuickTransfer() {
+function QuickTransfer({ creditCardNumber }) {
   return (
     <>
 
       <div className={styles.quickTransferBox}>
         <h1 className={styles.Title}>Quick Money Transfer</h1>
-        <div className={styles.cardBox}>
-          <img src={Tejarat} alt="its just an SUS logo is n't this weird?" className={styles.Tejarat} />
-          <span>5859 ***** ***** 2245</span>
-          <img src={Navigation} alt="its just a button LOL" className={styles.slider} />
-        </div>
+<CreditCardNumber  creditCardNumber={"5859 **** **** 2245"} />
         <button type='button' className={styles.confirmBtn}>Confirm Trasnfer</button>
       </div>
 
