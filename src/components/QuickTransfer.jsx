c5a2881a2838c2a7b4fede1cd903d102b9613571
Erfandev88 <gharibi69068@gmail.com>
2023-10-10 16:44:48 +0330

    Add confirmbtn

--- src/components/QuickTransfer.jsx
+++ src/components/QuickTransfer.jsx
@@ -6,12 +6,13 @@ function QuickTransfer() {
   return (
     <>
 
-      <div className={styles.quickTransferBox}> 
-     <h1 className={styles.Slider}>Quick Money Transfer</h1>
-       <div className={styles.cardBox}>
-        <img src={Navigation} alt="its just a button LOL" className={styles.slider}/>
-        <span>5859 ***** ***** 2245</span>
-        </div> 
+      <div className={styles.quickTransferBox}>
+        <h1 className={styles.Title}>Quick Money Transfer</h1>
+        <div className={styles.cardBox}>
+          <img src={Navigation} alt="its just a button LOL" className={styles.slider} />
+          <span>5859 ***** ***** 2245</span>
+        </div>
+        <button type='button' className={styles.confirmBtn}>Confirm Trasnfer</button>
       </div>
 
     </>
