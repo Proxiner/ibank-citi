b74ff4662788967182af77e204b465cff60adeb1
erfandev88 <erfangaaang@gmail.com>
2023-10-23 16:28:07 +0330

    recaptcha added

--- src/components/QuickTransfer.jsx
+++ src/components/QuickTransfer.jsx
@@ -31,7 +31,7 @@ function QuickTransfer() {
             <form onSubmit={handleSubmit}  className={styles.captchaImg}>
               <GoogleRecaptcha
                 ref={recaptchaRef}
-                sitekey="6LeQL8MoAAAAAD-m3yV7XsaRk6LamTg6ENg0jA1O"
+                sitekey="6LehUcMoAAAAACgnlUkR7vh7qoIWg4Bb03meJnWS"
               />
               <button type="submit">Submit</button>
             </form>
