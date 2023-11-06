4df632a182781495615a0a911cfcf6e2f55bcd57
erfandev88 <erfangaaang@gmail.com>
2023-11-06 16:39:14 +0330

    filterIcon added

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,16 +1,18 @@
 import React from "react";
 
-import styles from './_Search.module.scss';
+import styles from "./_Search.module.scss";
 
 const Search = ({ value, placeholder, onChange }) => {
   return (
-    <input
-      className={styles.search}
-      type="text"
-      placeholder={placeholder}
-      value={value}
-      onChange={onChange}
-    />
+    <div className={styles.searchContainer}>
+      <input
+        className={styles.search}
+        type="text"
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+      />
+    </div>
   );
 };
 
