b6e4bad40a05f243632cc2cd92e20f5fb33ace0f
Roham ghasemi <alijerban1388@gmail.com>
2023-11-16 20:14:32 +0330

    new packages has been added

---
+++ src/components/Transactions.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from './Transactions.module.scss'
+
+
+function Transactions() {
+  return (
+    <div>
+        <h1>Transactions</h1>
+        <h5>(recent)</h5>
+
+
+    </div>
+  )
+}
+
+export default Transactions
