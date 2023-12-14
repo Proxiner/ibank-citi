a6df637c3633d90f28794e58dd45a851197410b8
Roham ghasemi <alijerban1388@gmail.com>
2023-12-14 19:36:08 +0330

    1

--- src/layout/Transactions.jsx
+++ src/layout/Transactions.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./_Transactions.module.scss";
 import TransactionCard from "../components/TransactionCard";
 import dribbbleIcon from "../assets/dribble.svg";
@@ -7,6 +7,38 @@ import BeIcon from "../assets/be.svg";
 import NemIcon from "../assets/nem.svg";
 
 function Transactions() {
+  const [transactions, setTransactions] = useState([
+    {
+      iconBG: "#FF075C61",
+      imageSrc: dribbbleIcon,
+      title: "Dribbble",
+      date: "28 September 2022",
+      price: "$200,000 {USA}",
+    },
+    {
+      iconBG: "#1DD46061",
+      imageSrc: "spotifyIcon",
+      title: "Spotify",
+      date: "28 September 2022",
+      price: "$185,000,000",
+    },
+
+    {
+      iconBG: "#9791FC61",
+      imageSrc: "BeIcon",
+      title: "Dribbble",
+      date: "28 September 2022",
+      price: "$197,000",
+    },
+    {
+
+      iconBG: "##D0D61761",
+      imageSrc: "NemIcon",
+      title: "NEM (XEM)",
+      date: "28 September 2022",
+      price: "$30,000",
+    },
+  ])
   return (
     <>
       <section className={styles.transactions}>
@@ -15,7 +47,9 @@ function Transactions() {
           <span>(Recent)</span>
         </div>
         <div className={styles.cardContainer}>
-          <TransactionCard
+
+          return
+          {/* <TransactionCard
             iconBG={"#FF075C61"}
             imageSrc={dribbbleIcon}
             title={"Dribbble"}
@@ -42,7 +76,7 @@ function Transactions() {
             title={"NEM (XEM)"}
             date={"28 September 2022"}
             price={"$30,000"}
-          />
+          /> */}
         </div>
       </section>
       <section></section>
