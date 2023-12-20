56a19499d558f98d55b3526bf489e9b911493fe4
Roham ghasemi <alijerban1388@gmail.com>
2023-12-20 14:42:22 +0330

    -a

--- src/layouts/ChartSection.jsx
+++ src/layouts/ChartSection.jsx
@@ -1,28 +1,80 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styles from "./_Chart.module.scss";
 import Chart from "chart.js/auto";
 import { Bar } from "react-chartjs-2";
 // Chart Datas 
 
-function ChartSection() {
-  const labels = ["Dribbble", "Spotify", "Be`", "NEM"];
-  const data = {
-    labels: labels,
-    datasets: [
-      {
-        label: "Transactions",
-        backgroundColor: "#a29bfd",
-        borderColor: "rgb(255, 99, 132)",
-        data: [200000, 185000000, 197000, 30000],
-        tickLength: 20,
-      },
-    ],
-  };
-  return (
-    <div className={styles.chart}>
-      <Bar data={data} />
-    </div>
-  );
-}
 
-export default ChartSection;
+
+function  useChartDataSelection() {
+const [productPrice , setProductPrice] = useState([])
+  async function fetchData() {
+    const response = await fetch("https://dummyjson.com/products/")
+    const data = response.json()
+    const result = data
+    return result
+  }
+
+  const cards = useRef()
+  useEffect(() => {
+    async function fetchDataAndUpdate() {
+      const data = await fetchData()
+      const product =data.products.map((p ) => p.price);
+      setProductPrice(product)
+      return data
+    }
+
+    fetchDataAndUpdate()
+  }, [])
+
+}
+const options = {
+  plugins: { legend: { display: true } },
+  scales: { x: { grid: { display: false } }, y: { grid: { display: true } } },
+  datasets: { bar: { barPercentage: 8, categoryPercentage: 0.2 } },
+};
+
+
+  function ChartSection() {
+    const labels = ["Dribbble", "Spotify", "Behance", "NEM(IR)"];
+    const data = {
+      labels: labels,
+      datasets: [
+        {
+          label: "dribbble",
+          backgroundColor: "#FF075C",
+          borderColor: "rgb(255, 99, 132)",
+          data: [data.products[0].price, 0, 0, 0],
+          tickLength: 5,
+        },
+        {
+          label: "spotify",
+          backgroundColor: "#1DD460",
+          borderColor: "rgb(255, 99, 132)",
+          data: [0, data.products[1].price, 0, 0],
+          tickLength: 5,
+        },
+        {
+          label: "Behance",
+          backgroundColor: "#9791FC",
+          borderColor: "rgb(255, 99, 132)",
+          data: [0, 0, data.products[2].price, 0],
+          tickLength: 5,
+        },
+        {
+          label: "NEM(IR)",
+          backgroundColor: "#D0D617",
+          borderColor: "rgb(255, 99, 132)",
+          data: [0, 0, 0, data.products[3].data],
+          tickLength: 5,
+        },
+      ],
+    };
+    return (
+      <div className={styles.chart} >
+        <Bar data={data} options={options} />
+      </div>
+    );
+  }
+
+  export default ChartSection;
