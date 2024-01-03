bdb161a331cd0312c2b4ae0df99b51b757dac958
yasin <wolfsuper474@gmail.com>
2024-01-03 16:17:42 +0330

    -a

--- src/components/QuickTransfer.jsx
+++ src/components/QuickTransfer.jsx
@@ -20,6 +20,22 @@ function QuickTransfer() {
     recaptchaRef.current.getValue();
   };
 
+  
+  const swiperRef = useRef(null);
+
+  const handleNextSlide = () => {
+    if (swiperRef.current && swiperRef.current.swiper) {
+      swiperRef.current.swiper.slideNext();
+    }
+  };
+
+  const handlePrevSlide = () => {
+    if (swiperRef.current && swiperRef.current.swiper) {
+      swiperRef.current.swiper.slidePrev();
+    }
+  };
+
+
   return (
     <div className={styles.quickTransferBox}>
       <h1 className={styles.Title}>Quick Money Transfer</h1>
@@ -29,8 +45,9 @@ function QuickTransfer() {
         pagination={false}
         navigation={false}
         modules={[EffectFlip, Pagination, Navigation]}
-        className="swiper"
-        style={{ width: "400px", height: "70px" }}
+        ref={swiperRef}
+        className={styles.swiper}
+        style={{ width: "400px", height: "70px", position: 'relative' }}
       >
         <SwiperSlide>
           <CreditCardNumber creditCardNumber={"5859 **** **** 2245"} imageSrc={Tejarat} />
@@ -44,6 +61,74 @@ function QuickTransfer() {
         <SwiperSlide>
           <CreditCardNumber creditCardNumber={"5555 **** **** 5555"} imageSrc={Sepah}/>
         </SwiperSlide>
+
+        <button onClick={handlePrevSlide} className={styles.control}>
+            <svg
+              className={styles.arrowLeft}
+              id="vuesax_outline_arrow-circle-right"
+              data-name="vuesax/outline/arrow-circle-right"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+            >
+              <g id="arrow-circle-right">
+                <path
+                  id="Vector"
+                  d="M10.75,21.5A10.75,10.75,0,1,1,21.5,10.75,10.759,10.759,0,0,1,10.75,21.5Zm0-20A9.25,9.25,0,1,0,20,10.75,9.261,9.261,0,0,0,10.75,1.5Z"
+                  transform="translate(1.25 1.25)"
+                  fill="#e8e8e8"
+                />
+                <path
+                  id="Vector-2"
+                  data-name="Vector"
+                  d="M.747,8.558a.742.742,0,0,1-.53-.22.754.754,0,0,1,0-1.06l3-3-3-3A.75.75,0,0,1,1.278.218l3.53,3.53a.754.754,0,0,1,0,1.06l-3.53,3.53A.742.742,0,0,1,.747,8.558Z"
+                  transform="translate(9.992 7.723)"
+                  fill="#626262"
+                />
+                <path
+                  id="Vector-3"
+                  data-name="Vector"
+                  d="M0,0H24V24H0Z"
+                  fill="none"
+                  opacity="0"
+                />
+              </g>
+            </svg>
+          </button>
+          <button onClick={handleNextSlide} className={styles.control}>
+            <svg
+              id="vuesax_outline_arrow-circle-right"
+              data-name="vuesax/outline/arrow-circle-right"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+            >
+              <g id="arrow-circle-right">
+                <path
+                  id="Vector"
+                  d="M10.75,21.5A10.75,10.75,0,1,1,21.5,10.75,10.759,10.759,0,0,1,10.75,21.5Zm0-20A9.25,9.25,0,1,0,20,10.75,9.261,9.261,0,0,0,10.75,1.5Z"
+                  transform="translate(1.25 1.25)"
+                  fill="#e8e8e8"
+                />
+                <path
+                  id="Vector-2"
+                  data-name="Vector"
+                  d="M.747,8.558a.742.742,0,0,1-.53-.22.754.754,0,0,1,0-1.06l3-3-3-3A.75.75,0,0,1,1.278.218l3.53,3.53a.754.754,0,0,1,0,1.06l-3.53,3.53A.742.742,0,0,1,.747,8.558Z"
+                  transform="translate(9.992 7.723)"
+                  fill="#626262"
+                />
+                <path
+                  id="Vector-3"
+                  data-name="Vector"
+                  d="M0,0H24V24H0Z"
+                  fill="none"
+                  opacity="0"
+                />
+              </g>
+            </svg>
+          </button>
       </Swiper>
       <div className="cardboxContiner">
         <div className={styles.cardBox}>
