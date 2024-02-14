881ece70810d98b40a725a5fa0f0ff80d83581f2
yasin <wolfsuper474@gmail.com>
2024-02-14 17:39:51 +0330

    menuIcon added

--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -1,18 +1,40 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useRef, useState } from "react";
 import styles from "./Navbar.module.scss";
 import searchIcon from "../assets/search.svg";
 import sun from "../assets/sun.svg";
 import moon from "../assets/moon.svg";
 import { addCart } from "../pages/Home";
+import "../base/menu.css";
 
 function Navbar() {
   const { darkMode, handleModeChange } = useContext(addCart);
   const [darkModeToggler, setDarkModeToggler] = useState("4px");
 
+  const menuIcon = useRef();
+
+  const handleMenuClick = (e) => {
+    e.target.classList.toggle("opened");
+    e.target.setAttribute(
+      "aria-expanded",
+      e.target.classList.contains("opened")
+    );
+  };
   const handleClick = () => {
     setDarkModeToggler(darkModeToggler === "4px" ? "18px" : "4px");
     handleModeChange();
   };
+
+  // const menuDark = () => {
+  //   menuIcon.current.children[0].style.stroke = "white";
+  //   menuIcon.current.children[1].style.stroke = "white";
+  //   menuIcon.current.children[2].style.stroke = "white";
+  // };
+
+  // const menuLight = () => {
+  //   menuIcon.current.children[0].style.stroke = "black";
+  //   menuIcon.current.children[1].style.stroke = "black";
+  //   menuIcon.current.children[2].style.stroke = "black";
+  // };
 
   return (
     <nav>
@@ -77,23 +99,42 @@ function Navbar() {
             />
           </div>
         </form>
-        <div
-          className={`${styles.themeToggleBox} ${
-            darkMode ? "Navbar_darkMode__2jQk1" : ""
-          }`}
-        >
-          <button
-            className={`${styles.themeToggler} ${
+
+        <div className={styles.actionMobile}>
+          <div
+            className={`${styles.themeToggleBox} ${
               darkMode ? "Navbar_darkMode__2jQk1" : ""
             }`}
-            style={{ right: darkModeToggler }}
-            onClick={handleClick}
           >
-            {darkModeToggler === "4px" ? (
-              <img src={sun} className="sun" alt="Sun Icon" />
-            ) : (
-              <img src={moon} alt="Moon Icon" />
-            )}
+            <button
+              className={`${styles.themeToggler} ${
+                darkMode ? "Navbar_darkMode__2jQk1" : ""
+              }`}
+              style={{ right: darkModeToggler }}
+              onClick={handleClick}
+            >
+              {darkModeToggler === "4px" ? (
+                <img src={sun} className="sun" alt="Sun Icon" />
+              ) : (
+                <img src={moon} alt="Moon Icon" />
+              )}
+            </button>
+          </div>
+          <button class="menu" onClick={handleMenuClick} aria-label="Main Menu">
+            <svg width="50" height="50" viewBox="0 0 100 100">
+              <path
+                class={darkMode ? "lightmode line line1" : "line line1"}
+                d="M 20,29.000046 H 80.000231 C 80.000231,29.000046 94.498839,28.817352 94.532987,66.711331 94.543142,77.980673 90.966081,81.670246 85.259173,81.668997 79.552261,81.667751 75.000211,74.999942 75.000211,74.999942 L 25.000021,25.000058"
+              />
+              <path
+                class={darkMode ? "lightmode line line2" : "line line2"}
+                d="M 20,50 H 80"
+              />
+              <path
+                class={darkMode ? "lightmode line line3" : "line line3"}
+                d="M 20,70.999954 H 80.000231 C 80.000231,70.999954 94.498839,71.182648 94.532987,33.288669 94.543142,22.019327 90.966081,18.329754 85.259173,18.331003 79.552261,18.332249 75.000211,25.000058 75.000211,25.000058 L 25.000021,74.999942"
+              />
+            </svg>
           </button>
         </div>
       </div>
